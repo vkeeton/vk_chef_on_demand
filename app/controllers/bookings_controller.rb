77bd4a050b96a5447fbb1ad9cf95730a0948fe16
Zhen Luo <zhenluo1988@gmail.com>
2023-07-22 12:44:37 +0900

    added new and create action and new view for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,26 @@
 class BookingsController < ApplicationController
   def index
   end
+
+  def new
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:offer_id, :user_id, :service_date, :meals_quantity, :user_comment, :cook_at_hime)
 end
