354ae6b6b62627d1da7f94557f74c3e2a8e4cdb9
Zhen Luo <zhenluo1988@gmail.com>
2023-07-27 20:51:32 +0900

    changed registration and configuration and finished the new and create action for offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,4 +8,26 @@ class OffersController < ApplicationController
     @booking = Booking.new
     authorize @offer
   end
+
+  def new
+    @offer = Offer.new
+    authorize @offer
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+    authorize @offer
+  end
+end
+
+private
+
+def offer_params
+  params.require(:offer).permit(:user_id, :cuisine, :price)
 end
